9de37e18d9ae98db422b51296115430259a5b4ca
WillStephensonxyz <willstephenson66@gmail.com>
2026-03-17 14:26:02 +1100

    inital commit

--- i3-setup.sh
+++ i3-setup.sh
@@ -1,59 +1,101 @@
 #!/bin/bash
 
 # to do
-# configure terminal settings for urxvt
 # disable terminal bell
 # configure i3-status
-# configure gaps 
+# configure gaps (add to master config)
 # configure transparent terminal
-# configure custom keybind
-# fix setting background
+# import programs from kde (kdewallet) or replace
 
-PACKAGES=("i3" "i3status" "dmenu" "urxvt" "feh" "xmodmap")
+packages=("i3" "i3status" "dmenu" "picom" "rxvt-unicode" "feh" "xmodmap")
 
-CONFIG_FILE="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-kde/config"
-XRESOURCES="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-Xresources/.Xresources"
+i3_config_file="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-kde/config"
+xresources_config="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-Xresources/.Xresources"
+# picom_config=""
 
-CONFIG_DIR="$HOME/.config/i3"
-CONFIG_PATH="$CONFIG_DIR/config"
-XRESOURCES_PATH="$HOME/.Xresources"
-KACCESSRC="$HOME/.config/kaccessrc"
+config_dir="$HOME/.config/i3"
+config_path="$config_dir/config"
+xresources_path="$HOME/.Xresources"
+picom_dir="$HOME/.config/picom"
+picom_path="$HOME/.config/picom/picom.conf"
+kaccessrc="$HOME/.config/kaccessrc"
+xmodmap="$HOME/.Xmodmap"
 
-echo "Checking packages" 
-for PACKAGE in ${PACKAGES[@]}; 
-do
-	if rpm -q "${PACKAGE}" > /dev/null  2>&1; then
-		echo "$PACKAGE" already installed
+package_list() {  
+
+	echo "[*] Checking packages" 
+
+	for package in ${packages[@]}; 
+	do
+		if rpm -q "${package}" > /dev/null  2>&1; then
+			echo "$package" already installed
+		else
+			sudo dnf install -y "$package"
+			continue
+		fi
+	done
+} 
+
+configure_i3() {
+
+	echo "[*] Configuring i3"
+	if [[ -f "$config_path" ]]; then
+		echo "i3 config already exists"
 	else
-		sudo dnf install -y "$PACKAGE"
-		continue
+		echo "creating i3 config file"
+		mkdir -p "$config_dir"
+		curl -o "$config_path" "$i3_config_file"
 	fi
-done
+}
 
-echo "Configuring i3"
-if [[ -f "$CONFIG_PATH" ]]; then
-	echo "i3 config already exists"
-else
-	echo "creating i3 config file"
-	mkdir -p "$CONFIG_DIR"
-	curl -o "$CONFIG_PATH" "$CONFIG_FILE"
-fi
+configure_urxvt() {
 
-echo "Disable KDE bell" 
-if [[ -f "$KACCESSRC" ]] && grep -q "Systembell=true" "$KACCESSRC"; then 
-	sed -i "s/true/false/g" ~/.config/kaccessrc
-fi
+	echo "[*] Configuring urxvt"
+	if [[ -f "$xresources_path" ]]; then 
+		echo ".Xresoures already exists"
+	else
+		echo "creating .Xresources"
+		curl -o "$xresources_config" "$xresources_path"
+	fi
+}
 
-echo "Configuring urxvt"
-if [[ -f "$XRESOURCES_PATH" ]]; then 
-	echo ".Xresoures already exists"
-else
-	echo "creating .Xresources"
-	curl -o "$XRESOURCES_PATH" "$XRESOURCES"
-fi
+echo 
 
-echo "Configuring Xmodmap"
-if grep -q "keycode 107 = Super_L" "$XRESOURCES"; then 
-	echo "keycode 107 = Super_L" >> "$XRESOURCES"
-	xrdb merge "$XRESOURCES"
-fi
+configure_picom() {
+
+	echo "[*] Configuring picom"
+	if [[ ! -f "$picom_path" ]]; then
+		mkdir -p "$picom_dir" 
+		# curl -o "
+}
+
+configure_xmodmap() {
+
+	echo "[*] Configuring Xmodmap"	
+	if [[ ! -f "$xmodmap" ]]; then 
+		touch "$xmodmap"
+		echo "keycode 107 = Super_L" > "$xmodmap"
+		xmodmap "$xmodmap"
+	elif [[ -f "$xmodmap" ]] && ! grep -q "keycode 107 = Super_L" "$xmodmap"; then
+		echo "keycode 107 = Super_L" >> "$xmodmap"
+		xmodmap "$xmodmap"
+	fi 
+}
+
+kde_bell() {
+
+	echo "[*] Disabling KDE bell" 
+	if [[ -f "$kaccessrc" ]] && grep -q "SystemBell=true" "$kaccessrc"; then 
+		sed -i "s/true/false/g" ~/.config/kaccessrc
+	fi
+}
+
+main() {
+	package_list
+	configure_i3
+	configure_urxvt
+	configure_xmodmap
+	kde_bell
+}
+
+main
