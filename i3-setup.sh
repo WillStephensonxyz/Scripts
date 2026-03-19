2c9b97671704a7ecf12d623f86f50c58b6a6c603
WillStephensonxyz <willstephenson66@gmail.com>
2026-03-19 21:51:07 +1100

    refactored functions

--- i3-setup.sh
+++ i3-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # to do
-# disable terminal bell
 # configure i3-status
 # import programs from kde (kdewallet) or replace
-# test
+# refactor config downloads - turn into a case statement that checks, downloads config & mv to path
+# create preferred optional file structure
 
 packages=("i3" "i3status" "dmenu" "picom" "rxvt-unicode" "feh" "xmodmap")
 
@@ -12,86 +12,56 @@ i3_config="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/hea
 xresources_config="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-laptop-rice/i3-Xresources/.Xresources"
 picom_config="https://raw.githubusercontent.com/WillStephensonxyz/dotfiles/refs/heads/main/i3-laptop-rice/i3-picom/picom.conf"
 
-i3_dir="$HOME/.config/i3"
 i3_path="$HOME/.config/i3/config"
 xresources_path="$HOME/.Xresources"
-picom_dir="$HOME/.config/picom"
 picom_path="$HOME/.config/picom/picom.conf"
 kaccessrc="$HOME/.config/kaccessrc"
 xmodmap="$HOME/.Xmodmap"
 
-package_list() {  
-
-	echo "[*] Checking packages" 
-
-	for package in ${packages[@]}; 
-	do
-		if rpm -q "${package}" > /dev/null  2>&1; then
-			echo "$package" already installed
-		else
-			sudo dnf install -y "$package"
-			continue
-		fi
+package_list() {
+	echo "[*] Checking packages"
+	
+	for package in ${packages[@]}; do
+		rpm -q "$package" 2>/dev/null 2>&1 || {
+			echo "Installing "$package""
+			sudo dnf install "$package"
+		}
 	done
-} 
-
-configure_i3() {
-
-	echo "[*] Configuring i3"
-	if [[ ! -f "$i3_path" ]]; then
-		echo "creating i3 config file"
-		mkdir -p "$i3_dir"
-		curl -o "$i3_path" "$i3_config"
-	fi
 }
 
-configure_urxvt() {
+download_config() {
+	local config_file=="$1"
+	local download_url=="$2"
 
-	echo "[*] Configuring urxvt"
-	if [[ ! -f "$xresources_path" ]]; then
-		echo "creating .Xresources"
-		curl -o "$xresources_config" "$xresources_path" 
-	fi
+	[[ ! -f "$config_file" ]] && mkdir -p "$(dirname $config_file)" 
+	wget "$download_url" -O "$config_file" 
 }
 
-configure_picom() {
+keybinds() {
+	local line="keycode 107 = Super_L"
+	local xmodmap="$HOME/.Xmodmap"
 
-	echo "[*] Configuring picom"
-	if [[ ! -f "$picom_path" ]]; then
-		mkdir -p "$picom_dir" 
-		curl -o "$picom_path" "$picom_config"
-	fi
+	[[ -f "$xmodmap" ]] || touch "$xmodmap"
+	grep -q "$line" "$xmodmap" || echo "$line" >> "$xmodmap"
+	xmodmap "$xmodmap" 
 }
 
-configure_xmodmap() {
+# Optional directories
+[[ -d "$HOME"/Projects ]] || mkdir "$HOME/Projects"
+[[ -d "$HOME"/Study ]] || mkdir "$HOME/Study"
 
-	echo "[*] Configuring Xmodmap"	
-	if [[ ! -f "$xmodmap" ]]; then 
-		touch "$xmodmap"
-		echo "keycode 107 = Super_L" > "$xmodmap"
-		xmodmap "$xmodmap"
-	elif [[ -f "$xmodmap" ]] && ! grep -q "keycode 107 = Super_L" "$xmodmap"; then
-		echo "keycode 107 = Super_L" >> "$xmodmap"
-		xmodmap "$xmodmap"
-	fi 
-}
-
-kde_bell() {
-
-	echo "[*] Disabling KDE bell" 
-	if [[ -f "$kaccessrc" ]] && grep -q "SystemBell=true" "$kaccessrc"; then 
-		sed -i "s/true/false/g" ~/.config/kaccessrc
-	fi
-}
+# Disable the beep
+rmmod pcspkr
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
 main() {
-	package_list
-	configure_i3
-	configure_urxvt
-	configure_picom
-	configure_xmodmap
-	kde_bell
+	# package_list
+	# download config
+	keybinds
 	
 }
 
 main
+
+# echo "Packages and config files have been installed"
+# echo "Reccomend rebooting the machine to load changes"
