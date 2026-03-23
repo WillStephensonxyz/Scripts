0dd88855e5c6f6a2b13bbfa6e254c06727ae0b52
WillStephensonxyz <willstephenson66@gmail.com>
2026-03-23 17:34:08 +1100

    cleaned up functions

--- fedora-kde-i3-rice/i3-setup.sh
+++ fedora-kde-i3-rice/i3-setup.sh
@@ -1,67 +1,58 @@
 #!/bin/bash
+set -euo pipefail
 
 # to do
-# 
 # configure i3-status
 # delete native kde wallet program and replace with something else
 
 packages=("i3" "i3status" "dmenu" "picom" "rxvt-unicode" "feh" "xmodmap")
 
-kaccessrc="$HOME/.config/kaccessrc"
 xmodmap="$HOME/.Xmodmap"
 
-package_list() {
-	echo "[*] Checking packages"
-	
+download_packages() {
 	for package in ${packages[@]}; do
-		rpm -q "$package" 2>/dev/null 2>&1 || {
+		rpm -q "$package" &>/dev/null || {
 			echo "Installing "$package""
-			sudo dnf install "$package"
+			sudo dnf install -y "$package"
 		}
 	done
 }
 
+# Download config files from github repo
 download_config() {
-	local config_file="$HOME$1"
+	local config_file="$HOME/$1"
 	local url="$2"
 
 	[[ -f "$config_file" ]] && return 
 
-	mkdir -p "$(dirname $config_file)" 
+	mkdir -p "$(dirname "$config_file")" 
 	wget "$url" -O "$config_file" 
 }
 
-keybinds() {
-	local line="keycode 107 = Super_L"
-	local xmodmap="$HOME/.Xmodmap"
-
-	[[ -f "$xmodmap" ]] || touch "$xmodmap"
-	grep -q "$line" "$xmodmap" || echo "$line" >> "$xmodmap"
-	xmodmap "$xmodmap" 
+# Parses configs csv file and downloads config files
+apply_configs () {
+	while IFS=, read -r col1 col2; do
+		download_config "$col1" "$col2"
+	done < configs.csv
 }
+
+software() {
+	echo "[*] Checking packages"
+	download_packages
+	echo "[*] Applying configs" 
+	apply_configs
+}
+
+software
+
+# Remap PrtScr to Mod key
+[[ -f "$HOME/.Xmodmap" ]] || touch "$HOME/.Xmodmap"
+grep -qf "keycode 107 = Super_L" "$HOME/.Xmodmap" || echo "keycode 107 = Super_L" >> "$HOME/.Xmodmap"
+command xmodmap "$HOME/.Xmodmap"
 
 # Optional directories
-[[ -d "$HOME"/Projects ]] || mkdir "$HOME/Projects"
-[[ -d "$HOME"/Study ]] || mkdir "$HOME/Study"
-
-
-IFS=, 
-while read -r col1 col2; do
-	# echo "$HOME$col1" "$col2"
-	download_config "$col1" "$col2"
-done < configs.csv
+mkdir -p "$HOME/Projects" "$HOME/Study"
 
 # Disable the beep
-# rmmod pcspkr
-# sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
-
-main() {
-	package_list
-	download config
-	keybinds
-}
-
-# main
-
-# echo "Packages and config files have been installed"
-# echo "Reccomend rebooting the machine to load changes"
+rmmod pcspkr
+sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
